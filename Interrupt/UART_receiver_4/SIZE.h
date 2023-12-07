e2debbb125d2bfec2b13bf5dd8464cc5af8bda52
tsaida222da <128365762+tsaida222da@users.noreply.github.com>
2023-12-07 10:36:05 +0800

    Add files via upload

---
+++ Interrupt/UART_receiver_4/SIZE.h
@@ -0,0 +1,25 @@
+
+
+
+	  // task
+#define  TASKSIZE           2
+#define  STACKRAMBYTES   2500
+
+    // event and service
+#define  PENDSIZE           6
+#define  MEMORYPOOLBYTES    0
+#define  GETMEMORYSIZE      0
+#define  QSIZE              1    
+#define  MAILSIZE           1
+#define  FLAGSIZE           1	
+#define  MUTEXSIZE          1
+#define  UNTILSIZE          0
+
+    // padding part of task stack(selective)
+int PaddingOS[TASKSIZE+1] = {0};  
+#define  PADDINGIDLE       0      // idleTaskOS() padding
+
+
+
+
+

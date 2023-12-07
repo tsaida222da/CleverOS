74f5ed279f2ea8046b18bce3e648ba05acee7cf2
tsaida222da <128365762+tsaida222da@users.noreply.github.com>
2023-12-07 10:42:39 +0800

    Add files via upload

---
+++ StartError/flashSize_7/SIZE.h
@@ -0,0 +1,20 @@
+
+
+	  // task
+#define  TASKSIZE           1 
+#define  STACKRAMBYTES   1000
+
+    // event and service
+#define  PENDSIZE           5
+#define  MEMORYPOOLBYTES   48
+#define  GETMEMORYSIZE      2
+#define  QSIZE              2    
+#define  MAILSIZE           1
+#define  FLAGSIZE           1	
+#define  MUTEXSIZE          1
+#define  UNTILSIZE          2
+
+    // padding part of task stack(selective)
+int PaddingOS[TASKSIZE+1] = {0}; 
+#define  PADDINGIDLE       0      // idleTaskOS() padding
+

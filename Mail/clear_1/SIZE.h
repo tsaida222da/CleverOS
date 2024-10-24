f309acb1352da2364a05800da58eaa56704d0588
tsaida222da <128365762+tsaida222da@users.noreply.github.com>
2024-10-24 16:34:29 +0800

    Add files via upload

---
+++ Mail/clear_1/SIZE.h
@@ -0,0 +1,23 @@
+	
+
+
+	  // task
+#define  TASKSIZE          4 
+#define  STACKRAMBYTES     2500
+
+    // event and service
+#define  PENDSIZE           3
+#define  MEMORYPOOLBYTES    0
+#define  GETMEMORYSIZE      0
+#define  QSIZE              0    
+#define  MAILSIZE           1
+#define  FLAGSIZE           0	
+#define  MUTEXSIZE          0
+#define  UNTILSIZE          0
+
+    // padding part of task stack(selective)
+int PaddingOS[TASKSIZE+1] = {0};  
+#define  PADDINGIDLE       0      // idleTaskOS() padding
+#define  BULKBYTES        sizeof(long double)
+
+

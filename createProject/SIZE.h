28df7083bf431bd3957c8209da9fe99055697994
tsaida222da <128365762+tsaida222da@users.noreply.github.com>
2024-10-24 16:31:13 +0800

    Add files via upload

---
+++ createProject/SIZE.h
@@ -0,0 +1,20 @@
+
+	  // task
+#define  TASKSIZE          3 
+
+#define  STACKRAMBYTES     (CPUREGISTER + 40)*WORDSIZE*(TASKSIZE + 1)
+
+    // event and service
+#define  PENDSIZE           TASKSIZE
+#define  GETMEMORYSIZE      2 * TASKSIZE
+#define  QSIZE              TASKSIZE    
+#define  MAILSIZE           TASKSIZE
+#define  FLAGSIZE           TASKSIZE	
+#define  MUTEXSIZE          TASKSIZE
+#define  UNTILSIZE          TASKSIZE
+#define  MEMORYPOOLBYTES    TASKSIZE * 40
+
+    // padding part of task stack(selective)
+int PaddingOS[TASKSIZE+1] = {0};  
+#define  PADDINGIDLE       0      // idleTaskOS() padding
+#define  BULKBYTES        sizeof(long double)

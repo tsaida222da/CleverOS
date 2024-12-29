07e63b0cb956d77397dbe094da8904aba12d3715
tsaida222da <128365762+tsaida222da@users.noreply.github.com>
2024-12-29 15:24:17 +0800

    Add files via upload

---
+++ Interrupt/post_1/SIZE.h
@@ -0,0 +1,23 @@
+
+
+
+	  // task
+#define  TASKSIZE          5 
+#define  STACKRAMBYTES     2500
+
+    // event and service
+#define  PENDSIZE           4
+#define  MEMORYPOOLBYTES    0
+#define  GETMEMORYSIZE      0
+#define  QSIZE              1    
+#define  MAILSIZE           1
+#define  FLAGSIZE           1	
+#define  MUTEXSIZE          1
+#define  UNTILSIZE          0
+
+    // padding part of task stack(selective)
+int PaddingOS[TASKSIZE+1] = {0};  
+#define  PADDINGIDLE       0      // idleTaskOS() padding
+#define  BULKBYTES        sizeof(long double)
+
+
